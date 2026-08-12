61360a9d33b46fd8ea8bd4729baa32b499da0bb5
Richardjegede <adewalerichard750@gmail.com>
2026-08-12 21:10:37 +0100

    pay changes

--- app/api/paystack/verify/route.js
+++ app/api/paystack/verify/route.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { auth, db } from "../../lib/firebase";
+import { auth, db } from "../../../lib/firebase";
 import { doc, updateDoc, increment } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
 
